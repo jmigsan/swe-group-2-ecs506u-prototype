764dc15d1685b50e3f0fccfc594980afa620a4a8
jmigsan <43961001+jmigsan@users.noreply.github.com>
2024-03-12 16:40:10 +0000

    inexperienced ivnestor crypto list

---
+++ pages/inexperienced.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+
+const inexperienced = () => {
+  const [cryptocurrencies, setCryptocurrencies] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:3000/api/cryptocurrencies'
+        );
+        setCryptocurrencies(response.data.slice(0, 10)); //gets first 10 cryptos out of how many get received
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Cryptocurrency list</h2>
+      {cryptocurrencies.map((crypto, index) => (
+        <div>
+          <Link href={`http://localhost:3000/inexperienced/${crypto.ticker}`}>
+            <div key={index}>
+              <p>Name: {crypto.name}</p>
+              <p>Ticker: {crypto.ticker}</p>
+              <p>Price: £{crypto.price}</p>
+              <p>24hr Change: {crypto['24hr-change']}%</p>
+            </div>
+          </Link>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+export default inexperienced;
