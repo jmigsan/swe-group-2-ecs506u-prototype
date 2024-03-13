383acfe42c0360a2c228c998ecc4364b7be54266
jmigsan <43961001+jmigsan@users.noreply.github.com>
2024-03-13 12:42:57 +0000

    example component. will get deleted later.

---
+++ components/Example/Example.js
@@ -0,0 +1,10 @@
+import styles from './Example.module.css';
+
+const Example = () => {
+  return (
+    <div>
+      <div className={styles.exampleText}>Example</div>
+    </div>
+  );
+};
+export default Example;
