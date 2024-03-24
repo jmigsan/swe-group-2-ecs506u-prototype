7b16ee7892f4cee8a0384a6f64f5425b21afd485
heshamahmed <ec22518@qmul.ac.uk>
2024-03-24 16:33:13 +0000

    nav-bar fixed

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,10 +9,17 @@ import {motion} from 'framer-motion'
 import { useSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
 import walletIcon from '@/public/images/wallet.png'
+import chat from '@/public/images/chat.png'
+import convert from '@/public/images/convert.png'
+import create from '@/public/images/create.png'
+import bot from '@/public/images/bot.png'
+import posts from '@/public/images/posts.png'
+import searchFriends from '@/public/images/searchFriends.png'
+import trade from '@/public/images/trade.png'
+import ticket from '@/public/images/ticket.png'
 export default function NavBar(){
     const {data: session} = useSession();
-    const [cryptoHover, setCryptoHover] = useState(false);
-
+   
     const animations = {
     initial: {opacity:0},
     animate: {opacity:1},
@@ -93,9 +100,142 @@ export default function NavBar(){
                                                     </section>
                                         </motion.div>
                                 </div>
-                                <Link className={styles.link} href="/viewCryptos">Trade</Link>
-                                <Link className={styles.link} href="/feed">Feed</Link>
-                                <Link className={styles.link} href="/support">Support</Link>
+                                <div className={styles.dropDown}>
+                                        <Link className={styles.link} href="/viewCryptos">Trade</Link>
+                                        <motion.div variants={animations} initial="initial" animate="animate" transition={{duration:0.4, ease:"easeInOut"}} className={styles.toggleMenu}>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={convert}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                    </figure>
+                                                    <section className={styles.rightOption}>
+                                                    <section className={styles.boldText}>Convert Crypto</section>
+                                                    <section className={styles.subHeading}>Convert Crypto to other Crypto's</section>
+                                                    </section>
+                                            </section>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={trade}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                    </figure>
+                                                    <section className={styles.rightOption}>
+                                                    <section className={styles.boldText}>Trade on Charts</section>
+                                                    <section className={styles.subHeading}>View live price action as you trade</section>
+                                                    </section>
+                                            </section>
+                                        </motion.div>
+                                </div>
+                                <div className={styles.dropDown}>
+                                        <Link className={styles.link} href="/feed">Feed</Link>
+                                        <motion.div variants={animations} initial="initial" animate="animate" transition={{duration:0.4, ease:"easeInOut"}} className={styles.toggleMenu}>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={posts}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                    </figure>
+                                                    <section className={styles.rightOption}>
+                                                    <section className={styles.boldText}>View your Feed</section>
+                                                    <section className={styles.subHeading}>See your friends and following's posts</section>
+                                                    </section>
+                                            </section>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={create}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                    </figure>
+                                                    <section className={styles.rightOption}>
+                                                    <section className={styles.boldText}>Create Post</section>
+                                                    <section className={styles.subHeading}>Make your own post to share</section>
+                                                    </section>
+                                            </section>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={searchFriends}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                    </figure>
+                                                    <section className={styles.rightOption}>
+                                                    <section className={styles.boldText}>Search Friends</section>
+                                                    <section className={styles.subHeading}>Search for a friend</section>
+                                                    </section>
+                                            </section>
+                                        </motion.div>
+                                </div>
+                                <div className={styles.dropDown}>
+                                        <Link className={styles.link} href="/support">Support</Link>
+                                        <motion.div variants={animations} initial="initial" animate="animate" transition={{duration:0.4, ease:"easeInOut"}} className={styles.toggleMenu}>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={ticket}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                    </figure>
+                                                    <section className={styles.rightOption}>
+                                                    <section className={styles.boldText}>Ticket</section>
+                                                    <section className={styles.subHeading}>See your ongoing tickets and open a new one if needed</section>
+                                                    </section>
+                                            </section>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={chat}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                    </figure>
+                                                    <section className={styles.rightOption}>
+                                                    <section className={styles.boldText}>Chat</section>
+                                                    <section className={styles.subHeading}>Chat with a member of our support team</section>
+                                                    </section>
+                                            </section>
+                                            <section className={styles.menuOption}>
+                                                <figure className={styles.Icon}>
+                                                    <Image 
+                                                        src={bot}
+                                                        width={50}
+                                                        height={50}
+                                                        alt="Logo"
+                                                        className={styles.Icon}
+                                                    />
+                                                </figure>
+                                                    <section className={styles.rightOption}>
+                                                        <section className={styles.boldText}>Assistant</section>
+                                                        <section className={styles.subHeading}>Chat with our built in Chatbot</section>
+                                                    </section>
+                                            </section>
+                                        </motion.div>
+                                </div>
+                                
                                 </>
                             ): (
                                 <>
