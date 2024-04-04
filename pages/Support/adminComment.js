412b497e1ca950fae850fbf1a0049dc75d02195f
Ghafor <rahmatzada.abdulghafor@gmail.com>
2024-04-04 02:40:43 +0100

    The pop up to take admins comments

---
+++ pages/Support/adminComment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from '@/styles/support.module.css';
+
+const adminComment = ({ adminComments, setAdminComments, handleConfirmChange, onClose }) => {
+    return (
+        <div className={styles.modalBackground}>
+            <div className={styles.modalContent}>
+                <textarea
+                    className={styles.input}
+                    type="text"
+                    placeholder="Enter comments"
+                    onChange={(e) => setAdminComments(e.target.value)}
+                    value={adminComments}
+                />
+                <button
+                    className={styles.button}
+                    onClick={() => handleConfirmChange()}
+                >
+                    Confirm
+                </button>
+                <button onClick={onClose}>Go Back</button>
+            </div>
+        </div>
+    );
+};
+
+export default adminComment;
